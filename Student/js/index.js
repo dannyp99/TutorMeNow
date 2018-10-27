63072f1525da0b23fbe3c3537ca61608abce00b8
dannyp99 <dannypires99@gmail.com>
2018-10-27 16:03:42 -0400

    Empty Text Message Complete
    The alertify.error for all text fields are complete and working

--- Student/js/index.js
+++ Student/js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     var subject = document.getElementById('subject')
     var prof = document.getElementById('prof')
     var email = document.getElementById('email')
-
+/*
     if (!name.value || !scid.value|| !loc.value || !subject.value || !prof.value || !email.value) {
       alertify.error("Please check your entries");
       return false;
@@ -15,6 +15,35 @@ $(document).ready(function() {
       $(this).get(0).reset();
       alertify.success("Request Sent");
     }
+    */
+    if(!name.value){
+    	alertify.error("Please enter your Name");
+      	return false;
+    }
+    else if(!scid.value){
+      	alertify.error("Please enter your School I.D.");
+      	return false;
+    }
+    else if(!loc.value){
+      	alertify.error("Please choose a Tutoring Location");
+      	return false;
+    }
+    else if(!subject.value){
+      	alertify.error("Please choose a Subject");
+      	return false;
+    }
+    else if(!prof.value){
+      	alertify.error("Please enter your professor");
+      	return false;
+    }
+    else if(!email.value){ 
+      	alertify.error("Please enter your QC Student email");
+      	return false;
+    }else{
+      e.preventDefault();
+      $(this).get(0).reset();
+      alertify.success("Request Sent");
+   }
   });
 });
 
