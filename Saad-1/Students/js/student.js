5f614b05b08a62159f6261623ac8d882ba5e5dbc
SaadFaruque <32947579+SaadFaruque@users.noreply.github.com>
2018-10-28 01:21:55 -0400

    Organized documents
    Organized htmls and javascripts

--- Saad-1/Students/js/student.js
+++ Saad-1/Students/js/student.js
@@ -1,27 +1,23 @@
-
 $(document).ready(function() {
   $('#contact-form').submit(function(e) {
     var name    = document.getElementById('name')
     var scid = document.getElementById('scid')
+    var loc   = document.getElementById('location')
+    var subject = document.getElementById('subject')
     var prof = document.getElementById('prof')
     var email = document.getElementById('email')
 
-    if (!name.value) {
-      alertify.error("Please check your entries");
-      return false;
-    if (!scid.value) {
+    if (!name.value || !scid.value|| !loc.value || !subject.value || !prof.value || !email.value) {
       alertify.error("Please check your entries");
       return false;
     } else {
       e.preventDefault();
       $(this).get(0).reset();
-      var unique = uuid();
       alertify.success("Request Sent");
-    
     }
-      console.log(var unique)
-      
-      
-      
   });
 });
+
+
+
+
